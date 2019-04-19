ff92902b23f0816542a35136b747381f013bf872
jhonrsalcedo <jhonfor@gmail.com>
2019-04-19 16:50:59 -0500

    Guardando las categorias en state

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Formcategory from './components/Formcategory';
+
+
+// cosumimos la api eventbrite www.eventbrite.com
+class App extends Component {
+  constructor(props){
+    super(props);
+      this.state={
+        categoriesResp:[]
+      }
+  }
+    token = 'AC6B2UWEROHM2XESUDY7'
+  componentDidMount(){
+    this.getCategory();
+  }
+
+  //utilizamos el async
+  // getCategory = async () =>{
+  //     let url =`https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
+
+  //     await fetch(url)
+  //       .then(response =>{
+  //         return response.json();
+  //       })
+  //       .then(categories =>{
+  //         console.log(categories)
+  //       })
+  // }
+
+  // otra forma de utilizar async await
+  getCategory = async () =>{
+    const url =`https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
+    const response = await fetch(url)
+    const categoriesResp = await response.json()
+      //console.log(categoriesResp.categories) 
+      this.setState({
+        categoriesResp: categoriesResp.categories
+      })
+      
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header
+          title ='Eventos'
+        />
+        <Formcategory />
+      </div>
+    );
+  }
+}
+
+export default App;
